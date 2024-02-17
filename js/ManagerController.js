f8a7fae12697f4484e58de0d84fd13b1cbe6b4e0
Adrián <hyron529@gmail.com>
2024-02-17 19:15:00 +0100

    Añadimos el controlador y la vista a nuestra aplicacion

---
+++ js/ManagerController.js
@@ -0,0 +1,210 @@
+import { Coordinate } from "./Coordinate.js";
+
+const MODEL = Symbol('ManagerModel');
+const VIEW = Symbol('ManagerView');
+const LOAD = Symbol('Carga de objetos');
+
+class ManagerController {
+
+    /*
+        Constructor para el controlador que tiene por parámetro el modelo
+        y la vista
+        Definimos los eventos que queremos cargar y los iniciamos
+    */
+    constructor(model, view) {
+
+        this[MODEL] = model;
+        this[VIEW] = view;
+
+        this.onLoad();
+        this.onInit();
+        this[VIEW].bindInit(this.handleInit);
+    }
+
+    [LOAD]() {
+
+        //Creación de objetos de tipo plato
+        let plato1 = this[MODEL].createDish('Pisto manchego');
+        plato1.description = "Pisto casero con verduras cosechadas en nuestra tierra.";
+        plato1.ingredients = ["Pimiento rojo", "Pimiento verde", "Huevo frito", "Tomate frito"];
+        plato1.image = "./img/pisto.jpg";
+
+        let plato2 = this[MODEL].createDish('Migas');
+        plato2.description = "Migas de pastor, un plato tradicional elaborado con pan duro, panceta, chorizo y pimientos.";
+        plato2.ingredients = ["Pan", "Panceta", "Chorizo", "Pimientos"];
+        plato2.image = "./img/migas.jpg";
+
+        let plato3 = this[MODEL].createDish('Gazpacho manchego');
+        plato3.description = "Un guiso contundente de carne de caza, como liebre o conejo, acompañado de verduras y pan.";
+        plato3.ingredients = ["Carne de caza", "Verduras", "Pan"];
+        plato3.image = "./img/gazpacho_manchego.jpg";
+
+        let plato4 = this[MODEL].createDish('Atascaburras');
+        plato4.description = "Un puré de patatas con bacalao desmigado, ajo y nueces.";
+        plato4.ingredients = ["Patatas", "Bacalao", "Ajo", "Nueces"];
+        plato4.image = "./img/atascaburras.jpg";
+
+        let plato5 = this[MODEL].createDish('Morteruelo');
+        plato5.description = "Un paté manchego tradicional elaborado con carne de caza, pan, especias y manteca.";
+        plato5.ingredients = ["Carne de caza", "Pan", "Especias", "Manteca"];
+        plato5.image = "./img/morteruelo.jpg";
+
+        let plato6 = this[MODEL].createDish('Caldereta');
+        plato6.description = "Un guiso de cordero manchego con patatas y especias, típico de la región.";
+        plato6.ingredients = ["Cordero", "Patatas", "Especias"];
+        plato6.image = "./img/caldereta.jpg";
+
+        let plato7 = this[MODEL].createDish('Tiznao');
+        plato7.description = "Un plato elaborado con bacalao desmigado, pimientos, tomates y cebolla.";
+        plato7.ingredients = ["Bacalao", "Pimientos", "Tomates", "Cebolla"];
+        plato7.image = "./img/tiznao.jpg";
+
+        let plato8 = this[MODEL].createDish('Gachas manchegas');
+        plato8.description = "Un plato de harina de almortas o harina de trigo con agua, aceite y sal.";
+        plato8.ingredients = ["Harina de almortas", "Agua", "Aceite", "Sal"];
+        plato8.image = "./img/gachas_manchegas.jpg";
+
+        let plato9 = this[MODEL].createDish('Bizcocho de calabaza');
+        plato9.description = "Un delicioso bizcocho esponjoso elaborado con puré de calabaza, canela y nueces.";
+        plato9.ingredients = ["Puré de calabaza", "Harina", "Azúcar", "Canela", "Nueces"];
+        plato9.image = "./img/bizcocho_calabaza.jpg";
+
+        let plato10 = this[MODEL].createDish('Miguelitos de la Roda');
+        plato10.description = "Dulce hojaldrado relleno de crema pastelera y espolvoreado con azúcar glas.";
+        plato10.ingredients = ["Hoja de masa", "Crema pastelera", "Azúcar glas"];
+        plato10.image = "./img/miguelitos.jpg";
+
+        let plato11 = this[MODEL].createDish('Rosquillos de vino');
+        plato11.description = "Rosquillos crujientes hechos con vino y aromatizados con anís.";
+        plato11.ingredients = ["Harina", "Azúcar", "Vino", "Anís"];
+        plato11.image = "./img/rosquillos_vino.jpg";
+
+        let plato12 = this[MODEL].createDish('Gachas dulces');
+        plato12.description = "Un postre tradicional a base de harina de almortas, leche, azúcar y canela.";
+        plato12.ingredients = ["Harina de almortas", "Leche", "Azúcar", "Canela"];
+        plato12.image = "./img/gachas_dulces.jpg";
+
+
+        //Creación de objetos de tipo categoría
+        let cat1 = this[MODEL].createCategory('Tapas');
+        cat1.description = "Productos caseros de tapeo.";
+
+        let cat2 = this[MODEL].createCategory('Segundos');
+        cat2.description = "Platos tradicionales con productos manchegos.";
+
+        let cat3 = this[MODEL].createCategory('Postres');
+        cat3.description = "Los mejores postres de la comarca.";
+
+        //Creación de objetos de tipo restaurante
+        let rest1 = this[MODEL].createRestaurant("Gastromacha I");
+        rest1.description = "Disfruta de la gastronomia manchega en Ciudad Real."
+        rest1.location = new Coordinate("44.25", "33.33");
+
+        let rest2 = this[MODEL].createRestaurant("Gastromacha II");
+        rest2.description = "Disfruta de la gastronomia manchega en Toledo."
+        rest2.location = new Coordinate("53.99", "38.88");
+
+        let rest3 = this[MODEL].createRestaurant("Gastromacha III");
+        rest3.description = "Disfruta de la gastronomia manchega en Albacete."
+        rest3.location = new Coordinate("25.66", "29.10");
+
+        //Creación de objetos de tipo Alérgeno
+        let al1 = this[MODEL].createAllergen('Gluten');
+        let al2 = this[MODEL].createAllergen('Huevo');
+        let al3 = this[MODEL].createAllergen('Lactosa');
+        let al4 = this[MODEL].createAllergen('Pescado');
+
+        //Creación de objetos de tipo menú
+        let menu1 = this[MODEL].createMenu('Atacsamenú');
+        let menu2 = this[MODEL].createMenu('Vaya pisto de menú');
+        let menu3 = this[MODEL].createMenu('Menú guisos');
+
+        //Asignación de elementos
+
+        this[MODEL].addRestaurant(rest1, rest2, rest3);
+
+        this[MODEL].addAllergen(al1, al2, al3, al4);
+
+        this[MODEL].assignCategoryToDish(cat1, plato1, plato3, plato4, plato5);
+        this[MODEL].assignCategoryToDish(cat2, plato2, plato6, plato7, plato8);
+        this[MODEL].assignCategoryToDish(cat3, plato9, plato10, plato11, plato12);
+
+        this[MODEL].assignAllergenToDish(al1, plato2, plato5, plato8, plato9, plato10, plato11, plato12);
+        this[MODEL].assignAllergenToDish(al2, plato1, plato9);
+        this[MODEL].assignAllergenToDish(al3, plato12);
+        this[MODEL].assignAllergenToDish(al4, plato7, plato4);
+
+        this[MODEL].assignDishToMenu(menu1, plato1, plato2, plato6, plato11);
+        this[MODEL].assignDishToMenu(menu2, plato3, plato7, plato12, plato9);
+        this[MODEL].assignDishToMenu(menu3, plato4, plato5, plato8, plato10);
+
+        /*
+            Creción de métodos para cargar e iniciar los métodos definidos
+            además de los manejadores de eventos necesarios para el funcionamiento
+            adecuado de nuestra página 
+        */
+        onLoad = () => {
+            this[LOAD]();
+            this.onAddCategory();
+            this.onAddAllergen();
+            this.onAddMenu();
+            this.onAddRestaurant();
+        };
+
+        onInit = () => {
+            this[VIEW].showCategories(this[MODEL].categories);
+            this[VIEW].bindDishCategoryList(this.handleDishCategoryList);
+            this[VIEW].showRandomDishes(this[MODEL].getRandomDishes());
+            this[VIEW].bindRandomDishes(this.handleDishRandomList);
+        }
+
+        handleInit = () => {
+            this.onInit();
+        }
+
+        handleDisplayDish = (name) => {
+            const dish = this[MODEL].createDish(name);
+            this[VIEW].displayDish(dish);
+        }
+
+        handleDisplayRestaurant = (name) => {
+            const restaurant = this[MODEL].createRestaurant(name);
+            this[VIEW].displayRestaurant(restaurant);
+        }
+
+        handleRandomDishes = (name) => {
+            const dish = this[MODEL].createDish(name);
+            this.handleDisplayDish(dish.name);
+        }
+
+        handleDishInCategory = (name) => {
+            const category = this[MODEL].createCategory(name);
+            this[VIEW].listDishes(
+                this[MODEL].getDishesInCategory(category),
+                category.name
+            );
+            this[VIEW].bindShowDish(this.handleDisplayDish);
+        }
+
+        handleDishInAllergen = (name) => {
+            const allergen = this[MODEL].createAllergen(name);
+            this[VIEW].listDishes(
+                this[MODEL].getDishesWithAllergen(allergen),
+                allergen.name
+            );
+            this[VIEW].bindShowDish(this.handleDisplayDish);
+        };
+
+        handleDishInMenu = (name) => {
+            const menu = this[MODEL].createMenu(name);
+            this[VIEW].listDishes(
+                this[MODEL].getDishesInMenu(menu),
+                menu.name
+            );
+            this[VIEW].bindShowDish(this.handleDisplayDish);
+        }
+
+    }
+}
+
+export default ManagerController;
---
+++ js/ManagerView.js
@@ -0,0 +1,137 @@
+
+class ManagerView {
+    /*
+        Constructor donde vamos a iniciar la ubicación de las
+        zonas donde vamos a trabajar
+    */
+    constructor() {
+
+        this.inicio = document.getElementById("inicio");
+        this.central = document.getElementById("central");
+        this.menu = document.querySelector(".menu");
+
+    }
+
+    /*
+        Manejador de eventos para cuando hacemos click 
+        en inicio o en el logo
+    */
+    bindInit(handler) {
+        document.getElementById("inicio").addEventListener("click", (event) => {
+            handler();
+        });
+
+        document.getElementById("logo").addEventListener("click", (event) => {
+            handler();
+        });
+    }
+
+    /*
+        Método que nos va a permitir visualizar las categorías del restaurante
+        Primero se elimina la fila que contiene a las categorias, y definimos un div que las va a contener
+        Le ponemos un título para un mejor diseño y ponemos las categorías
+        Una vez hecho, colocamos el div en en html
+    */
+    showAllCategories(cat) {
+
+        if (this.centralZone.children.length > 0)
+            this.centralZone.children[0].remove();
+
+        const catCont = document.createElement("div");
+
+        catCont.classList.add("row");
+        catCont.id = "dishes";
+        catCont.insertAdjacentHTML(
+            "beforeend",
+            `
+                <h3>Todas las categorías de nuestros productos<h3>
+            `
+        );
+
+        for (const category of categories) {
+
+            catCont.insertAdjacentHTML(
+
+                "beforeend",
+                `
+                    <div class="card black rounded-3 mt-20" style="margin-right: 40px; margin-bottom: 40px width: 10%">
+                        <a data-category="${category.category.name}" href="#dishes">
+                            <div class="div-body text-center">
+                                <p class="div-content">${category.category.name}
+                                </p>
+                            </div>
+                        </a>
+                    </div>
+                `
+            );
+        }
+        this.centralZone.append(catCont);
+    }
+
+    /*
+        Método para desplegar las categoría en el nav de nuestra página
+        Definimos la lista y el dropdown
+        Ponemos el dorpdown en el enlace que las contendrá y definimos el ul
+        Recorremos las categorías para poder mostrarlas en el desplegable en forma de lista
+        y añadimos todos los elementos
+    */
+    showNavBarCategories(cat) {
+
+        const item = document.createElement("li");
+
+        item.classList.add("navBar-item", "dropdown");
+
+        item.insertAdjacentHTML(
+            "beforeend",
+            `
+                <a id="navBarCat" class="navBar-click dropdown-toggle text-black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorías</a>
+            `
+        );
+
+        const cats = document.createElement("ul");
+        cats.classList.add("dropdown-menu", "bg-dark");
+
+        for (const category of categories) {
+            cats.insertAdjacentHTML(
+                "beforeend",
+                `
+                <li>
+                    <a class="dropdown-item rounded text-secondary data-category="${category.category.name}
+                        href="#productlist" style="width: 80%;>
+                        ${category.category.name}
+                    </a>
+                </li>
+                `
+            );
+        }
+        item.append(cats);
+        this.menu.append(item);
+    }
+
+    /*
+        Manejador de eventos que captura cuando hacemos click en una categoría
+        Recogemos el elemento donde se hace el click y el enlace
+        Recorremos los enlaces y recogemos el valor seleccionado
+    */
+    bindCategoryList(handler) {
+
+        const categoryList = document.getElementById("dishes");
+        const links = categoryList.querySelectorAll("a");
+
+        for (const link of links) {
+            link.addEventListener("click", (event) => {
+                handler(event.currentTarget.dataset.category);
+                console.log(event.currentTarget.dataset.category);
+            });
+        }
+    }
+
+    randomDishes(dish){
+
+        if (this.initZone.children.length > 0) this.initZone.children[0].remove();
+
+        const div = document.createElement("div");
+    }
+
+
+}  
